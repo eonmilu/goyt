b7745f3abcce182edebd716e7dbe6f1e2ae14dfc
eonmilu <eonmilu@gmail.com>
2018-08-20 17:30:54 +0200

    Rename main.go to goyt.go

--- goyt.go
+++ goyt.go
@@ -29,3 +29,18 @@ type YourTime struct {
 	// DB is the database where the Timemarks and users are stored
 	DB *sql.DB
 }
+
+// Timemark is the data structure for a timemark
+type Timemark struct {
+	TimemarkID int64  `json:"timemarkID"`
+	Author     string `json:"author"`
+	AuthorURL  string `json:"authorURL"`
+	Timemark   int64  `json:"timemark"`
+	Content    string `json:"content"`
+	Votes      int64  `json:"votes"`
+	Date       int64  `json:"date"`
+}
+
+type timemarksDB struct {
+	*sql.DB
+}
