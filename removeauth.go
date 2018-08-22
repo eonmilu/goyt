182541c79b08332cb0312395348013354030f1bd
eonmilu <eonmilu@gmail.com>
2018-08-22 22:16:28 +0200

    Remove redundancy on the token
    The token was being sent twice (by POST and by cookies)

--- removeauth.go
+++ removeauth.go
@@ -4,13 +4,16 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"regexp"
+	"strings"
 )
 
 // RemoveAuth reads a token passed by HTTPS POST and changes the DB's entry
 // to an empty string
 func (y YourTime) RemoveAuth(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	token := getToken(r)
+	cookies := r.Header.Get("Cookie")
+	re := regexp.MustCompile(`(?m)yourtime-token-server=.*[^\]|;]`)
+	token := strings.Split(re.FindAllString(cookies, 1)[0], "=")[1]
 
 	_, err := y.DB.Exec("UPDATE users SET token=$1 WHERE token=$2", "", token)
 	if err != nil {
