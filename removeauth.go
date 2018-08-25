0f0f7e4cb5e85110eb5d374c3d60b2c23e27a26d
eonmilu <eonmilu@gmail.com>
2018-08-25 16:36:15 +0200

    Validate arrays before checking them

--- removeauth.go
+++ removeauth.go
@@ -25,6 +25,7 @@ func (y YourTime) RemoveAuth(w http.ResponseWriter, r *http.Request) {
 
 	cookie := http.Cookie{
 		Name:    "yourtime-token-server",
+		Path:    "/",
 		Value:   "",
 		Expires: time.Unix(0, 0),
 		Secure:  true,
@@ -36,7 +37,14 @@ func (y YourTime) RemoveAuth(w http.ResponseWriter, r *http.Request) {
 func getTokenFromCookies(r *http.Request) token {
 	cookies := r.Header.Get("Cookie")
 	re := regexp.MustCompile(`(?m)yourtime-token-server=.*[^\]|;]`)
-	tkn := strings.Split(re.FindAllString(cookies, 1)[0], "=")[1]
-
-	return token(tkn)
+	cookie := re.FindAllString(cookies, 1)
+	var tokens []string
+	if len(cookie) > 0 {
+		tokens = strings.Split(cookie[0], "=")
+	}
+	tkn := token("")
+	if len(tokens) >= 1 {
+		tkn = token(tokens[1])
+	}
+	return tkn
 }
