963f51a84355d73602bd16c676b47ea4ec3a8690
eonmilu <eonmilu@gmail.com>
2018-11-01 22:15:25 +0100

    Select "identifier" instead of "email" from the database

--- votes.go
+++ votes.go
@@ -209,7 +209,7 @@ func (y YourTime) getUserIdentifier(r *http.Request) (string, error) {
 
 func (y YourTime) getEmailFromToken(tkn token) (string, error) {
 	email := ""
-	row := y.DB.QueryRow("SELECT email FROM users WHERE token=$1", string(tkn))
+	row := y.DB.QueryRow("SELECT identifier FROM users WHERE token=$1", string(tkn))
 	err := row.Scan(&email)
 	if err != nil {
 		return "", err
